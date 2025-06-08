3f732edf3bc94d4d09a0e38383a76e3ecfcadd5f
milewsk <milewski.konrad00@gmail.com>
2025-06-08 19:56:32 +0200

    Add ArchitectureTests proj

---
+++ tests/ArchitectureTests/BaseTest.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using Application.Abstractions.Messaging;
+using Domain.Users;
+using Infrastructure.Data;
+using Web.Controllers;
+
+namespace ArchitectureTests;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly DomainAssembly = typeof(User).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(ICommand<>).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(ApplicationDbContext).Assembly;
+    protected static readonly Assembly WebAssembly = typeof(UserController).Assembly;
+}
